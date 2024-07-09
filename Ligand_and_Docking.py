0d71b4da6be9c49c1e156cd68be7a2676df79517
Gavin Joiner <joinergavin66@gmail.com>
2024-07-09 16:57:00 -0400

    Add files via upload

---
+++ Ligand_and_Docking.py
@@ -0,0 +1,38 @@
+import requests 
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+# SMILES string for compound 
+SMILES = input("")
+pdb_id = input("")
+
+# Add explicit hydrogens, and generate molecule from SMILES
+mol = Chem.MolFromSmiles(SMILES)
+mol = Chem.AddHs(mol)
+
+# Generate 3D coordinates
+AllChem.EmbedMolecule(mol, randomSeed=42)
+AllChem.UFFOptimizeMolecule(mol)
+
+# Save the 3D coordinates to a PDB file
+SMILES_file_path = "{SMILES}.pdb"
+Chem.MolToPDBFile(mol, SMILES_file_path)
+
+print("SMILES:" + SMILES)
+print(f"3D structure saved to {SMILES_file_path}")
+
+# URL to fetch the PDB file
+pdb_url = f"https://files.rcsb.org/download/{pdb_id}.pdb"
+
+# Fetch the PDB file
+r = requests.get(pdb_url)
+
+# Check if the request was successful
+if 200 <= r.status_code <= 299:
+    pdb_file_path = f"{pdb_id}.pdb" 
+    with open(pdb_file_path, 'w') as file:
+        file.write(r.text)
+    print(f"{pdb_id} saved as {pdb_file_path}")
+else:
+    print(f"Failed to download: {pdb_id}")
